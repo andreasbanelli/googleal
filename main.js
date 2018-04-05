2e5d72dfe1e01fae8b652d92d5345073aa450977
andreasbanelli <andreas.banelli@gmail.com>
2018-04-05 18:43:57 +0200

    first commi

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    document.querySelector(".centerbox").style.visibility = "visible"; 
+
+    butonHide.style.display = 'none';
+}
+
+function mySearch (){
+
+let go = "http://www.google.fr/#q="
+raw = document.getElementById('exampleInputEmail1').value;
+var replaced = raw.replace(/ /g, '+');
+let url = go + replaced
+open (url)
+}
+
+
+    setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
